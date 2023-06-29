b44e735072e9b2bed1f2829e80ab2ec6d456fe79
Sivajayam <siva.jayam18@gmail.com>
2023-06-28 19:50:13 -0500

    second commit

--- Team20.bat
+++ Team20.bat
@@ -2,10 +2,10 @@ call newman run "Batch Module.postman_collection.json" -d Postman_Hackathon_Data
 
 echo Program report generated
 
-call newman run "Assignment Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
+call newman run "Assignment Module.postman_collection.json" -g "C:\\Users\\sivas\\OneDrive\\Documents\\Postman\\workspace.postman_globals.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
 
 echo Batch report generated
 
-call newman run "Program Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/ProgramReport1.html
+call newman run "Program Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
 
 echo Assignment report generated
