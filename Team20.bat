b11696c3159d0b1d011ea7b29cdeaabd0b1fff24
Sivajayam <siva.jayam18@gmail.com>
2023-06-28 19:29:01 -0500

    Third commit

---
+++ Team20.bat
@@ -0,0 +1,11 @@
+call newman run Batch Module.postman_collection.json -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/ProgramReport1.html
+
+echo Program report generated
+
+call newman run Assignment Module.postman_collection.json -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/BatchReport1.html
+
+echo Batch report generated
+
+call newman run Program Module.postman_collection.json -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
+
+echo Assignment report generated
