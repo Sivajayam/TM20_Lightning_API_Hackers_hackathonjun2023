b247d2908be5fa425ed726ffabc57a03ac06a9ac
Sivajayam <siva.jayam18@gmail.com>
2023-06-29 07:34:31 -0500

    FinalBatchFileCommit

--- Team20.bat
+++ Team20.bat
@@ -1,11 +1,20 @@
-call newman run "Batch Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/BatchReport1.html
+call newman run "Program Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/ProgramReport1.html
 
 echo Program report generated
 
-call newman run "Assignment Module.postman_collection.json" -g "C:\\Users\\sivas\\OneDrive\\Documents\\Postman\\workspace.postman_globals.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
+call newman run "Batch Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/BatchReport1.html
 
 echo Batch report generated
 
-call newman run "Program Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/ProgramReport1.html
+call newman run "User Module - Pritee.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/UserReport1.html
+
+echo user report generated
+
+call newman run "Assignment Module.postman_collection.json" -g "C:\\Users\\sivas\\OneDrive\\Documents\\Postman\\workspace.postman_globals.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentReport1.html
 
 echo Assignment report generated
+
+call newman run "Assignment Submit Module.postman_collection.json" -d Postman_Hackathon_Data.csv -r htmlextra --reporter-htmlextra-export ./reports/AssignmentSubmitReport1.html
+
+echo Assignmentsubmit report generated
+
